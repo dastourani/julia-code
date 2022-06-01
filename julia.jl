de551e31adb848b0135b66e3910923ebcc11e6dd
Hamed <dastourani.0@gmail.com>
2022-06-02 04:16:45 +0430

    GA

--- julia.jl
+++ julia.jl
@@ -1,1 +1,14 @@
-test
+using Random
+using StatsBase
+
+nQueens = 4
+popSize = 5
+
+function initPop(nQueens, popSize)
+    return [ rand(1:nQueens, nQueens) for i in 1:popSize ]
+end
+
+
+parent = initPop(nQueens, popSize)
+childSize = 2
+map(x -> x[ sample(1:nQueens, childSize, replace=false) ], parent )
