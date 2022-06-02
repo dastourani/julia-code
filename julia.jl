515e8c46bd25dd039f2e15baa113f8876c92d3ec
Hamed <dastourani.0@gmail.com>
2022-06-02 14:28:22 +0430

    last edit

--- julia.jl
+++ julia.jl
@@ -1,14 +1,7 @@
 using Random
-using StatsBase
 
-nQueens = 4
-popSize = 5
 
 function initPop(nQueens, popSize)
     return [ rand(1:nQueens, nQueens) for i in 1:popSize ]
 end
-
-
-parent = initPop(nQueens, popSize)
-childSize = 2
-map(x -> x[ sample(1:nQueens, childSize, replace=false) ], parent )
+initPop(5, 4)
